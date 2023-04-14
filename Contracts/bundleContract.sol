d7f640b3e8c9902be6025e47b3d5a28d294fcba1
technocrat-x <89021435+Technocrat-X@users.noreply.github.com>
2023-04-14 11:15:06 +0200

    Direct mint, remove use of SafeMath, remove checks

--- Contracts/bundleContract.sol
+++ Contracts/bundleContract.sol
@@ -1,14 +1,11 @@
-// This is the smart contract for the token bundle. It is initialized by the bundleFactory.sol contract
+// This smart contract allows you to create a bundle of tokens. The bundle is made out of base tokens in various proportions that can be minted and burned by the users, but follows the ERC20 standard for any other purpose.
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract bundleContract is ERC20 {
-    using SafeMath for uint256;
-
     address[] public tokens;
     uint256[] public proportions;
     uint256 public totalProportions;
@@ -19,34 +16,35 @@ contract bundleContract is ERC20 {
         address[] memory _tokens,
         uint256[] memory _proportions
     ) ERC20(name, symbol) {
-        require(
-            _tokens.length == _proportions.length,
-            "Tokens and proportions arrays must have the same length"
-        );
         tokens = _tokens;
         proportions = _proportions;
 
         for (uint256 i = 0; i < _proportions.length; i++) {
-            totalProportions = totalProportions.add(_proportions[i]);
+            totalProportions += _proportions[i];
         }
     }
 
-    function mint(uint256 amount) external {
-        require(tokens.length > 0, "No tokens in the bundle");
-
+    // This function allows you to mint some amount of the token bundle. The tokens must first be sent to the bundleContract.
+    function mint(uint256 amount, address recipient) external {
+        _mint(recipient, amount);
+    }
+    
+    // This function allows you to mint some amount of the token bundle. The tokens must first be approved to be spent by the bundleContract on the behalf of msg.sender
+    function mintFrom(uint256 amount, address recipient) external {
         for (uint256 i = 0; i < tokens.length; i++) {
-            uint256 tokenAmount = amount.mul(proportions[i]).div(totalProportions);
+            uint256 tokenAmount = amount * proportions[i] / totalProportions;
             IERC20(tokens[i]).transferFrom(msg.sender, address(this), tokenAmount);
         }
-        _mint(msg.sender, amount);
+        _mint(recipient, amount);
     }
 
-    function burn(uint256 amount, address to) external {
+    // This function allows you to burn some amount of token bundle. The tokens will be sent to the recipient.
+    function burn(uint256 amount, address recipient) external {
         _burn(msg.sender, amount);
 
         for (uint256 i = 0; i < tokens.length; i++) {
-            uint256 tokenAmount = amount.mul(proportions[i]).div(totalProportions);
-            IERC20(tokens[i]).transfer(to, tokenAmount);
+            uint256 tokenAmount = amount * proportions[i] / totalProportions;
+            IERC20(tokens[i]).transfer(recipient, tokenAmount);
         }
     }
 }
